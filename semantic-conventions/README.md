62cc0f305c91dfba3e0fadaf90ec2160b1ad72be
Giovanni Liva <giovanni.liva@dynatrace.com>
2020-08-27 16:09:04 +0200

    Semantic convention generator (#5)
    * First cut of SemConvGen
    
    * Add Github Action
    
    * Address feedback
    
    * Make output more close to what we currently use

---
+++ semantic-conventions/README.md
@@ -0,0 +1,98 @@
+# Semantic Convention generator + Docker
+
+A docker image to process Semantic Convention YAML models.
+
+
+# Usage
+
+The image can be used to generate Markdown tables or code.
+
+```bash
+docker run --rm -v<yaml-path>:<some-path> -v<output-path>:<some-path> otel/semconvgen [OPTION]
+```
+
+For help try:
+
+```bash
+docker run --rm otel/semconvgen -h
+```
+
+## Markdown Tables
+
+Tables can be generated using the command:
+
+```bash
+docker run --rm otel/semconvgen --yaml-root {yaml_folder} markdown --markdown-root {markdown_folder}
+```
+
+Where `{yaml_folder}` is the absolute path to the directory containing the yaml files and
+`{markdown_folder}` the absolute path to the directory containing the markdown definitions
+(`specification` for [opentelemetry-specification](https://github.com/open-telemetry/opentelemetry-specification/tree/master/)).
+
+The tool will automatically replace the tables with the up to date definition of the semantic conventions.
+To do so, the tool looks for special tags in the markdown.
+
+```
+<!-- semconv {semantic_convention_id} -->
+<!-- endsemconv -->
+```
+
+Everything between these two tags will be replaced with the table definition.
+The `{semantic_convention_id}` MUST be the `id` field in the yaml files of the semantic convention
+for which we want to generate the table.
+After `{semantic_convention_id}`, optional parameters enclosed in parentheses can be added to customize the output:
+
+- `tag={tag}`: prints only the attributes that have `{tag}` as a tag;
+- `full`: prints attributes and constraints inherited from the parent semantic conventions or from included ones;
+- `ref`: prints attributes that are referenced from another semantic convention;
+- `remove_constraint`: does not print additional constraints of the semantic convention.
+
+### Examples
+
+These examples assume that a semantic convention with the id `http.server` extends another semantic convention with the id `http`.
+
+`<!-- semconv http.server -->` will print only the attributes and constraints of the `http.server` semantic
+convention.
+
+`<!-- semconv http.server(full) -->` will print the attributes and constraints of the `http` semantic
+convention and also the attributes and constraints of the `http.server` semantic convention.
+
+`<!-- semconv http.server() -->` is equivalent to `<!-- semconv http.server -->`.
+
+`<!-- semconv http.server(tag=network) -->` will print the constraints and attributes of the `http.server` semantic
+convention that have the tag `network`.
+
+`<!-- semconv http.server(tag=network, full) -->` will print the constraints and attributes of both `http` and `http.server`
+semantic conventions that have the tag `network`.
+
+## Code Generator
+
+The image supports [Jinja](https://jinja.palletsprojects.com/en/2.11.x/) templates to generate code from the models.
+
+For example, the following template is used by the [opentelemetry-java-instrumentation](https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/master/library-api/src/main/java/io/opentelemetry/instrumentation/api/typedspan) to generate Java classes.
+[Template Link.](https://gist.github.com/thisthat/7e34742f4a7f1b5df57118f859a19c3b)
+
+The image can generate code with the following command:
+
+```bash
+docker run --rm otel/semconvgen --yaml-root {yaml_folder} code --template {jinja-file} --output {output-file}
+```
+
+By default, all models are fed into the specified template at once, i.e. only a single file is generated.
+This is helpful to generate constants for the semantic attributes, [example from opentelemetry-java](https://github.com/open-telemetry/opentelemetry-java/blob/master/api/src/main/java/io/opentelemetry/trace/attributes/SemanticAttributes.java).
+
+If the parameter `--file-per-group {pattern}` is set, a single yaml model is fed into the template
+and the value of `pattern` is resolved from the model and attached as prefix to the output argument.
+This way, multiple files are generated. The value of `pattern` can be one of the following:
+- `semconv_id`: The id of the semantic convention.
+- `prefix`: The prefix with which all attributes starts with.
+- `extends`: The id of the parent semantic convention.
+
+Finally, additional value can be passed to the template in form of `key=value` pairs separated by
+comma using the `--parameters [{key=value},]+` flag.
+
+For example, to generate the typed spans used by the [opentelemetry-java-instrumentation](https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/master/library-api/src/main/java/io/opentelemetry/instrumentation/api/typedspan), the following command can be used:
+
+```bash
+docker run --rm otel/semconvgen --yaml-root ${yamls} code --template typed_span_class.java.j2 --file-per-group semconv_id -o ${output}/Span.java
+```
---
+++ semantic-conventions/src/opentelemetry/semconv/templating/code.py
@@ -0,0 +1,137 @@
+#   Copyright The OpenTelemetry Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import datetime
+import os.path
+import re
+import typing
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+from opentelemetry.semconv.model.semantic_convention import (
+    SemanticConventionSet,
+    SemanticConvention,
+)
+from opentelemetry.semconv.model.utils import ID_RE
+
+
+def to_doc_brief(doc_string: typing.Optional[str]) -> str:
+    if doc_string is None:
+        return ""
+    doc_string = doc_string.strip()
+    if doc_string.endswith("."):
+        return doc_string[:-1]
+    return doc_string
+
+
+def merge(list: typing.List, elm):
+    return list.extend(elm)
+
+
+def to_const_name(name: str) -> str:
+    return name.upper().replace(".", "_").replace("-", "_")
+
+
+def to_camelcase(name: str, first_upper=False) -> str:
+    first, *rest = name.replace("_", ".").split(".")
+    if first_upper:
+        first = first.capitalize()
+    return first + "".join(word.capitalize() for word in rest)
+
+
+class CodeRenderer:
+    pattern = "{{{}}}".format(ID_RE.pattern)
+    matcher = re.compile(pattern)
+
+    parameters: typing.Dict[str, str]
+
+    @staticmethod
+    def from_commandline_params(parameters=None):
+        if parameters is None:
+            parameters = []
+        params = {}
+        if parameters:
+            for elm in parameters:
+                pairs = elm.split(",")
+                for pair in pairs:
+                    (k, v) = pair.split("=")
+                    params[k] = v
+        return CodeRenderer(params)
+
+    def __init__(self, parameters: typing.Dict[str, str]):
+        self.parameters = parameters
+
+    def get_data_single_file(
+        self, semconvset: SemanticConventionSet, template_path: str
+    ) -> dict:
+        """Returns a dictionary that contains all SemanticConventions to fill the template."""
+        data = {
+            "template": template_path,
+            "semconvs": semconvset.models,
+            "attributes": semconvset.attributes(),
+        }
+        data.update(self.parameters)
+        return data
+
+    def get_data_multiple_files(
+        self, semconv: SemanticConvention, template_path: str
+    ) -> dict:
+        """Returns a dictionary with the data from a single SemanticConvention to fill the template."""
+        data = {"template": template_path, "semconv": semconv}
+        data.update(self.parameters)
+        return data
+
+    @staticmethod
+    def setup_environment(env: Environment):
+        env.filters["to_doc_brief"] = to_doc_brief
+        env.filters["to_const_name"] = to_const_name
+        env.filters["merge"] = merge
+        env.filters["to_camelcase"] = to_camelcase
+
+    @staticmethod
+    def prefix_output_file(file_name: str, pattern: str, semconv: SemanticConvention):
+        base = os.path.basename(file_name)
+        dir = os.path.dirname(file_name)
+        value = getattr(semconv, pattern)
+        return os.path.join(dir, to_camelcase(value, True), base)
+
+    def render(
+        self,
+        semconvset: SemanticConventionSet,
+        template_path: str,
+        output_file,
+        pattern: str,
+    ):
+        file_name = os.path.basename(template_path)
+        folder = os.path.dirname(template_path)
+        env = Environment(
+            loader=FileSystemLoader(searchpath=folder),
+            autoescape=select_autoescape([""]),
+        )
+        self.setup_environment(env)
+        if pattern:
+            semconv: SemanticConvention
+            for semconv in semconvset.models.values():
+                output_name = self.prefix_output_file(output_file, pattern, semconv)
+                data = self.get_data_multiple_files(semconv, template_path)
+                template = env.get_template(file_name, data)
+                template.globals["now"] = datetime.datetime.utcnow()
+                template.globals["version"] = os.environ.get("ARTIFACT_VERSION", "dev")
+                template.stream(data).dump(output_name)
+        else:
+            data = self.get_data_single_file(semconvset, template_path)
+            template = env.get_template(file_name, data)
+            template.globals["now"] = datetime.datetime.utcnow()
+            template.globals["version"] = os.environ.get("ARTIFACT_VERSION", "dev")
+            template.stream(data).dump(output_file)

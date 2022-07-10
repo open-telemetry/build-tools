b14e33475dacb53328f3d2b93b3c90d147b2cabe
Joao Grassi <joao@joaograssi.com>
2022-07-10 14:49:45 +0200

    Fix linter issues

--- semantic-conventions/src/tests/semconv/templating/test_code.py
+++ semantic-conventions/src/tests/semconv/templating/test_code.py
@@ -23,15 +23,21 @@ def test_codegen_units(test_file_path, read_test_file):
     assert result == expected
 
 
-@pytest.mark.parametrize("trim_blocks, lstrip_blocks", [("True", "True"), ("true", "TRUE"), ("TRUE", "tRuE")])
-def test_strip_blocks_enabled(test_file_path, read_test_file, trim_blocks, lstrip_blocks):
+@pytest.mark.parametrize(
+    "trim_blocks, lstrip_blocks", [("True", "True"), ("true", "TRUE"), ("TRUE", "tRuE")]
+)
+def test_strip_blocks_enabled(
+    test_file_path, read_test_file, trim_blocks, lstrip_blocks
+):
     """Tests that Jinja config options are passed to the Jinja environment"""
     semconv = SemanticConventionSet(debug=False)
     semconv.parse(test_file_path("yaml", "metrics", "units.yaml"))
     semconv.finish()
 
     template_path = test_file_path("jinja", "metrics", "units_template")
-    renderer = CodeRenderer({}, {"trim_blocks": trim_blocks, "lstrip_blocks": lstrip_blocks})
+    renderer = CodeRenderer(
+        {}, {"trim_blocks": trim_blocks, "lstrip_blocks": lstrip_blocks}
+    )
 
     output = io.StringIO()
     renderer.render(semconv, template_path, output, None)

081329473b048b09eacb6e4103877d2c85e02e37
Liudmila Molkova <limolkova@microsoft.com>
2023-09-27 10:37:24 -0700

    black

--- semantic-conventions/src/opentelemetry/semconv/model/semantic_convention.py
+++ semantic-conventions/src/opentelemetry/semconv/model/semantic_convention.py
@@ -126,12 +126,15 @@ class BaseSemanticConvention(ValidatableYamlNode):
         if not hasattr(self, "attrs_by_name"):
             return []
 
-        return sorted([
-            attr
-            for attr in self.attrs_by_name.values()
-            if templates is None
-            or templates == AttributeType.is_template_type(attr.attr_type)
-        ], key=lambda attr: attr.fqn)
+        return sorted(
+            [
+                attr
+                for attr in self.attrs_by_name.values()
+                if templates is None
+                or templates == AttributeType.is_template_type(attr.attr_type)
+            ],
+            key=lambda attr: attr.fqn,
+        )
 
     def __init__(self, group):
         super().__init__(group)

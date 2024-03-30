766c8afb319b2df0f6c28039cbf0038e2e947903
Liudmila Molkova <limolkova@microsoft.com>
2024-03-29 20:48:44 -0700

    up

--- semantic-conventions/src/opentelemetry/semconv/templating/markdown/__init__.py
+++ semantic-conventions/src/opentelemetry/semconv/templating/markdown/__init__.py
@@ -55,7 +55,6 @@ class RenderContext:
         self.group_key = ""
         self.enums = []
         self.notes = []
-        self.units = []
         self.current_md = ""
         self.current_semconv = None
 
@@ -299,17 +298,6 @@ class MarkdownRenderer:
 
             for attr in sampling_relevant_attrs:
                 output.write("* " + self.render_fqn_for_attribute(attr) + "\n")
-
-    @staticmethod
-    def to_markdown_unit_table(members, output: io.StringIO):
-        output.write("\n")
-        output.write(
-            "| Name        | Kind of Quantity         | Unit String   |\n"
-            "| ------------| ----------------         | -----------   |"
-        )
-        for member in members.values():
-            output.write(f"\n| {member.id} | {member.brief} | `{member.value}` |")
-        output.write("\n")
 
     def to_markdown_enum(self, output: io.StringIO):
         """Renders enum types after a Semantic Convention Table
